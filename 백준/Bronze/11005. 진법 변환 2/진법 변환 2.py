58e2865124b0d941f5d18ee8f3fe2d9ad2c4d900
JEMIN-stack <skyjejemin@gmail.com>
2025-10-19 18:47:20 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,26 +1,11 @@
-N,B = map(int, input().split())
-N_10_dic = {}
+N, B = map(int, input().split())
+digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
-for k in range(10):
-    N_10_dic[str(k)] = k
-for i in range(26):
-    N_10_dic[chr(65+i)] = 10+i  
-
-def change_N_to_B(N,B): 
-    B_list = []
-    result = ''
-    while True:
-        k = N % B
-        B_list.append(k)
-        N = N // B 
-        if N == 0 :
-            break
-    B_list.reverse()
-    for i in B_list:
-        for s in N_10_dic:
-            if i == N_10_dic[s]:
-                result += str(s)
-    return result
-    
-print(change_N_to_B(N,B))
+if N == 0:
+    print(0)
+else:
+    out = []
+    while N > 0:
+        out.append(digits[N % B])
+        N //= B
+    print(''.join(reversed(out)))
