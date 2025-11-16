90f402a20cacb1a6269e3c32271b38f129c5ef73
JEMIN-stack <skyjejemin@gmail.com>
2025-11-16 22:56:33 +0900

    [Bronze I] Title: 최소공배수, Time: 64 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(void){
+    int T;
+    int A,B;
+    int *pa = &A;
+    int *pb = &B;
+    
+    scanf("%d", &T);
+
+    for(int i=0; i<T; i++){
+        scanf("%d %d",pa,pb);
+        if (*pa==*pb){
+            printf("%d\n",*pa);
+        }
+        else if (*pa % *pb == 0){
+            printf("%d\n",*pa);
+        }
+        else if (*pb % *pa == 0){
+            printf("%d\n",*pb);
+        }
+        else {
+            int B_real = *pb;
+            int i = 2;
+           
+            for (i=2; i < B_real/2; i++){
+                while (*pb % i == 0){
+                    *pb = *pb / i;
+                    if (*pa % i == 0){
+                        *pa = *pa / i;
+                    }
+                }
+            }
+            printf("%d\n",(*pa)*B_real);
+        }
+    }
+
+    return 0;
+}
