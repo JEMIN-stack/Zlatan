16045ae06e57c24542cb62d7d45101f845206453
JEMIN-stack <skyjejemin@gmail.com>
2026-05-17 23:35:15 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int hiki(int x, int y) {
+    return !y ? x : hiki(x ^ y, (~x & y) << 1);
+}
+
+int solution(int num1, int num2) {
+    int n = hiki(num1, num2);
+    return n;
+}
+
+int main() {
+    cout << solution(2,3) << endl;
+    cout << solution(100,2) << endl;
+
+    return 0;
+}
