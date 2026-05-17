2011a66f453009192d578397933e150547b900bc
JEMIN-stack <skyjejemin@gmail.com>
2026-05-18 00:13:45 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 * num2;
+    return answer;
+}
+
+int main() {
+    cout << solution(3,4) << endl;
+    cout << solution(27,19) << endl;
+
+    return 0;
+}
