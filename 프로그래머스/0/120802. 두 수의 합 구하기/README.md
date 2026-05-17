0b95e18f796c53162668df70b8f922fa132cbd4a
JEMIN-stack <skyjejemin@gmail.com>
2026-05-18 00:08:20 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합 구하기/README.md
+++ 프로그래머스/0/120802. 두 수의 합 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 두 수의 합 구하기 - 120802 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120802) 
 
 ### 성능 요약
 
-메모리: 4 MB, 시간: 0.01 ms
+메모리: 3.94 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 17일 22:57:24
+2026년 05월 18일 00:08:17
 
 ### 문제 설명
 
--- 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int hiki(int x, int y) {
+    return !y ? x : hiki(x ^ y, (x & y) << 1);
+}
+
 int solution(int num1, int num2) {
-    int answer = num1 + num2;
-    return answer;
+    int n = hiki(num1, num2);
+    return n;
 }
 
 int main() {
     cout << solution(2,3) << endl;
-    cout << solution(100,200) << endl;
+    cout << solution(100,2) << endl;
 
     return 0;
 }
