0852fa1f09aa43cf0d871d02510856b6967142f0
JEMIN-stack <skyjejemin@gmail.com>
2026-05-18 16:37:25 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.cpp
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int solution(int num1, int num2) {
-    int answer = num1 * num2;
-    return answer;
+int result = 0;
+int hiki(int x, int y, int result) {
+    if (y == 0)
+	return result;
+    
+    else if ((y & 1) == 1)
+        result += x;
+
+    return hiki(x << 1, y >> 1, result); 
+}
+
+int solution(int num1, int num2) { 
+    int n = hiki(num1, num2, 0);
+    return n;
 }
 
 int main() {
@@ -12,3 +23,4 @@ int main() {
 
     return 0;
 }
+
